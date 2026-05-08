23a0313072800cdc52f574eb425165b44f538642
bsbinfoweb-arch <bsbinfoweb@gmail.com>
2026-05-08 18:50:29 -0400

    Update Supabase URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Remplace ces valeurs par celles de ton projet Supabase.
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://rdyesypoeahtqrpeoilv.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://rdyesypoeahtqrpeoilv.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_ATqg0oxw-6RO1rCNNK4XRg_EoAkLPMT"
 };
